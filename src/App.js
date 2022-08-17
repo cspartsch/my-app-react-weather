aa368ab90da7b924b406c9b1f0a651cacae862ea
cspartsch <101918784+cspartsch@users.noreply.github.com>
2022-08-16 21:15:58 -0700

    Testing

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ function App() {
   return (
     <div className="App">
       <h1>Weather App!</h1>
-      <h2>Today was a good day!</h2>
+      <h2>Today was a good day! Let's try again</h2>
       <footer>
         This project was coded by Carolyn Partsch and is open-sourced on{" "}
-        <a href="https://github.com/cspartsch/react-weather" target="_blank">
+        <a href="https://github.com/cspartsch/react-weather" target="#blank">
           GitHub
         </a>
       </footer>
