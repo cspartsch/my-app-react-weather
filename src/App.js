e7c04cbbdb9c16188bcff7dacc27f238f7e4d501
cspartsch <101918784+cspartsch@users.noreply.github.com>
2022-08-16 21:03:22 -0700

    Cleaned up file for coding.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <h1>Weather App!</h1>
+      <h2>Today was a good day!</h2>
+      <footer>
+        This project was coded by Carolyn Partsch and is open-sourced on{" "}
+        <a href="https://github.com/cspartsch/react-weather" target="_blank">
+          GitHub
         </a>
-      </header>
+      </footer>
     </div>
   );
 }
