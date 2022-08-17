433bd1712a838d8a5eeb1b9d3f6f7e4d59f17c73
cspartsch <101918784+cspartsch@users.noreply.github.com>
2022-08-16 21:26:19 -0700

    Added bootstrap to project

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import "./App.css";
 
+
 function App() {
   return (
     <div className="App">
-      <h1>Weather App!</h1>
-      <h2>Today was a good day! Let's try again</h2>
-      <footer>
-        This project was coded by Carolyn Partsch and is open-sourced on{" "}
-        <a href="https://github.com/cspartsch/react-weather" target="#blank">
-          GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App!</h1>
+        <footer>
+          This project was coded by Carolyn Partsch and is open-sourced on{" "}
+          <a href="https://github.com/cspartsch/react-weather" target="#blank">
+            GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
