739bdd8d8ee7d59f086cfc4f5c92334a028d04b5
cspartsch <101918784+cspartsch@users.noreply.github.com>
2022-08-21 14:41:11 -0700

    Updated Github link

--- src/App.js
+++ src/App.js
@@ -5,10 +5,14 @@ function App() {
   return (
     <div className="App">
       <div className="container">
+        <h1>Weather Search</h1>
         <Weather defaultCity="New York" />
         <footer className="footer">
           This project was coded by Carolyn Partsch and is open-sourced on{" "}
-          <a href="https://github.com/cspartsch/react-weather" target="#blank">
+          <a
+            href="https://github.com/cspartsch/my-app-react-weather"
+            target="#blank"
+          >
             GitHub
           </a>
         </footer>
