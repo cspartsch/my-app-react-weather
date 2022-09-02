e99d1a470487f52cc2005b4c4708ffdde52e91b8
cspartsch <101918784+cspartsch@users.noreply.github.com>
2022-09-01 21:16:23 -0700

    Weather Forecast mapping

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./WeatherForecast.css";
 import axios from "axios";
 import WeatherForecastDay from "./WeatherForecastDay";
@@ -18,9 +18,14 @@ export default function WeatherForecast(props) {
       <div className="card">
         <div className="WeatherForecast">
           <div className="row">
-            <div className="col">
-              <WeatherForecastDay data={forecast[0]} />
-            </div>
+            {forecast.map(function (dailyForecast, index) {
+              if (index < 5)
+                return (
+                  <div className="col" key={index}>
+                    <WeatherForecastDay data={dailyForecast} />
+                  </div>
+                );
+            })}
           </div>
         </div>
       </div>
